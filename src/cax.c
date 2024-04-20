dd9f26122ef1d0c74494baba5d8a11b10faa6eea
Ankit Kumar Sahu <sahuankit.murhu@gmail.com>
2024-04-21 02:52:25 +0530

    Adding error throwing

--- src/cax.c
+++ src/cax.c
@@ -1,20 +1,36 @@
+/*** Headerfiles ***/
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+/*** Data***/
+
 struct termios originalTemios;
+
+/*** Terminal ***/
+
+
+void die(const char *s ){
+    perror(s);
+    exit(1);
+}
 
 // it resets the terminal to original state
 void disableRawMode(){
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &originalTemios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &originalTemios) == -1)
+        die("tcsetattr");
 }
 
 // By default terminal is set to canonical mode which send input after pressing enter
 void enableRawMode()
 {
-    tcgetattr(STDIN_FILENO, &originalTemios);
+    if(tcgetattr(STDIN_FILENO, &originalTemios) == -1)
+        die("tcgetattr");
+
     atexit(disableRawMode); //it calls disableRawMode function automatically when program exits
 
     struct termios raw = originalTemios;
@@ -31,21 +47,23 @@ void enableRawMode()
     raw.c_cflag |= (CS8);
 
     // ECHO make each keypress to be printed in the terminal, which is not of our use
-    //ICANON make the program quit when 'q' is clicked
+    // ICANON make the program quit when 'q' is clicked
     // IEXTEN is used to disable ctrl + v
-    //ISIG is used to disable ctrl + c and ctrl + z
+    // ISIG is used to disable ctrl + c and ctrl + z
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN |ISIG); 
 
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die("tcsetattr");
 }
+
+/*** Init ***/
 
 int main()
 {
     enableRawMode();
-    char c;
 
     // read() returns the number of bytes that it read, and will return 0 when it reaches the end of a file.
     // program quits when 'q' is pressed
@@ -53,7 +71,7 @@ int main()
     // while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
     while (1){
         char c = '\0';
-        read(STDIN_FILENO , &c , 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if(iscntrl(c))
             printf("%d\r\n" , c);
         
