4a8bfa0793eb0d012317226e0148996dfab833e4
Ankit Kumar Sahu <sahuankit.murhu@gmail.com>
2024-05-17 00:22:40 +0530

    Adding home, end and delete keys

--- src/cax.c
+++ src/cax.c
@@ -19,6 +19,9 @@ enum editorKey{
   ARROW_RIGHT , 
   ARROW_UP ,
   ARROW_DOWN, 
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
   PAGE_UP,
   PAGE_DOWN
 };
@@ -122,10 +125,20 @@ int editorReadKey()
           return '\x1b';
         if(seq[2] == '~'){
           switch (seq[1]) {
+            case '1':
+              return HOME_KEY;
+            case '3':
+              return DEL_KEY;
+            case '4':
+              return END_KEY;
             case '5':
               return PAGE_UP;
             case '6':
               return PAGE_DOWN;
+            case '7':
+              return HOME_KEY;
+            case '8':
+              return END_KEY;
           }
         }
       } else{
@@ -135,13 +148,25 @@ int editorReadKey()
             return ARROW_UP;
           case 'B' :
             return ARROW_DOWN;
-          case 'c':
+          case 'C':
             return ARROW_RIGHT;
-          case 'd':
+          case 'D':
             return ARROW_LEFT;
+          case 'H':
+            return HOME_KEY;
+          case 'F':
+            return END_KEY;
         }
       }
+    } else if(seq[0] == 'O'){
+      switch (seq[1]) {
+        case 'H': 
+          return HOME_KEY;
+        case 'F':
+          return END_KEY;
+      }
     }
+  
     return '\x1b';
   }
   else{
@@ -350,6 +375,14 @@ void editorProcessKeypress()
     // repositions cursor before exit
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
+    break;
+
+  case HOME_KEY:
+    E.cx = 0;
+    break;
+
+  case END_KEY:
+    E.cx = E.screenCols - 1;
     break;
 
   case PAGE_UP:
