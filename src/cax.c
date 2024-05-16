d842f6dd35f21e5677b664c85c5033afa853555e
Ankit Kumar Sahu <sahuankit.murhu@gmail.com>
2024-05-17 00:07:35 +0530

    Adding Pageup and Pagedown keys

--- src/cax.c
+++ src/cax.c
@@ -18,7 +18,9 @@ enum editorKey{
   ARROW_LEFT = 1000 , 
   ARROW_RIGHT , 
   ARROW_UP ,
-  ARROW_DOWN 
+  ARROW_DOWN, 
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 /*** Data***/
@@ -114,18 +116,32 @@ int editorReadKey()
       return '\x1b';
 
     if(seq[0] == '['){
-      switch (seq[1]) {
-        case 'A' : 
-          return ARROW_UP;
-        case 'B' :
-          return ARROW_DOWN;
-        case 'c':
-          return ARROW_RIGHT;
-        case 'd':
-          return ARROW_LEFT;
+    
+      if(seq[1] >= '0' && seq[1] <= '9'){
+        if(read(STDIN_FILENO, &seq[2] , 1) != 1)
+          return '\x1b';
+        if(seq[2] == '~'){
+          switch (seq[1]) {
+            case '5':
+              return PAGE_UP;
+            case '6':
+              return PAGE_DOWN;
+          }
+        }
+      } else{
+
+        switch (seq[1]) {
+          case 'A' : 
+            return ARROW_UP;
+          case 'B' :
+            return ARROW_DOWN;
+          case 'c':
+            return ARROW_RIGHT;
+          case 'd':
+            return ARROW_LEFT;
+        }
       }
     }
-
     return '\x1b';
   }
   else{
@@ -336,6 +352,14 @@ void editorProcessKeypress()
     exit(0);
     break;
 
+  case PAGE_UP:
+  case PAGE_DOWN:
+    {
+      int times = E.screenRows;
+      while(times--)
+        editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    }
+    break;
   case ARROW_UP:
   case ARROW_DOWN:
   case ARROW_LEFT:
