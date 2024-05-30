2c4b014215f8f27b76868a5c8e1ba6929f4bb73d
kmr-ankitt <sahuankit.murhu@gmail.com>
2024-05-30 09:06:32 +0530

    Refractor syntax highlighting

--- src/cax.c
+++ src/cax.c
@@ -37,6 +37,12 @@ enum editorKey{
   PAGE_DOWN
 };
 
+
+enum editorHighlight {
+  HL_NORMAL = 0,
+  HL_NUMBER
+};
+
 /*** Data***/
 
 // It stores a row of text
@@ -45,6 +51,7 @@ typedef struct erow{
   int rsize;
   char *chars;
   char *render;
+  unsigned char *hl;
 }erow;
 
 /* Here we are configuring the terminal window */
@@ -256,6 +263,27 @@ int getWindowSize(int *rows, int *cols)
   }
 }
 
+
+/*** syntax highlighting ***/
+
+void editorUpdateSyntax(erow *row) {
+  row->hl = realloc(row->hl, row->rsize);
+  memset(row->hl, HL_NORMAL, row->rsize);
+  int i;
+  for (i = 0; i < row->rsize; i++) {
+    if (isdigit(row->render[i])) {
+      row->hl[i] = HL_NUMBER;
+    }
+  }
+}
+
+int editorSyntaxToColor(int hl) {
+  switch (hl) {
+    case HL_NUMBER: return 31;
+    default: return 37;
+  }
+}
+
 /*** Row operations ***/
 
 int editorRowCxToRx(erow *row, int cx) {
@@ -300,6 +328,8 @@ void editorUpdateRow(erow *row) {
   }
   row->render[idx] = '\0';
   row->rsize = idx;
+  editorUpdateSyntax(row);
+
 }
 
 
@@ -318,6 +348,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 
   E.row[at].rsize = 0;
   E.row[at].render = NULL;
+  E.row[at].hl = NULL;
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
@@ -327,6 +358,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 void editorFreeRow(erow *row) {
   free(row->render);
   free(row->chars);
+  free(row->hl);
 }
 
 void editorDelRow(int at) {
@@ -610,7 +642,7 @@ void editorDrawRows(struct abuf *ab)
         int padding = (E.screenCols - welcomelen) / 2;
         if(padding){
           abAppend(ab , "~" , 1);
-          padding--;
+      padding--;
         }
         while(padding--)
           abAppend(ab , " ", 1);
@@ -619,24 +651,37 @@ void editorDrawRows(struct abuf *ab)
       }else {
         abAppend(ab, "~", 1);
       }
+
     } else {
       int len = E.row[filerow].rsize - E.coloff;
-      if(len<0)
-        len = 0;
-      if(len > E.screenCols) 
-        len = E.screenCols;
+      if (len < 0) len = 0;
+      if (len > E.screenCols) len = E.screenCols;
       char *c = &E.row[filerow].render[E.coloff];
+      unsigned char *hl = &E.row[filerow].hl[E.coloff];
+      int current_color = -1;
       int j;
       for (j = 0; j < len; j++) {
-        if (isdigit(c[j])) {
-          abAppend(ab, "\x1b[31m", 5);
+        if (hl[j] == HL_NORMAL) {
+          if (current_color != -1) {
+            abAppend(ab, "\x1b[39m", 5);
+            current_color = -1;
+          }
           abAppend(ab, &c[j], 1);
-          abAppend(ab, "\x1b[39m", 5);
         } else {
+          int color = editorSyntaxToColor(hl[j]);
+          if (color != current_color) {
+            current_color = color;
+            char buf[16];
+            int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);
+            abAppend(ab, buf, clen);
+          }
           abAppend(ab, &c[j], 1);
         }
       }
+      abAppend(ab, "\x1b[39m", 5);
     }
+
+
     // Clears each line as we withdraw them
     abAppend(ab , "\x1b[K" , 3 );
     // drawing last line
